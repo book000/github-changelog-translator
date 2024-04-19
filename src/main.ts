12e081f5762e352eb464b28e343f5d3ba49d67fe
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2024-04-19 19:53:54 +0900

    feat: support eslint v9

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,37 @@ import { XMLBuilder, XMLParser } from 'fast-xml-parser'
 import fs from 'node:fs'
 import path from 'node:path'
 
+interface Feed {
+  rss: {
+    '@_xmlns:content': string
+    '@_xmlns:wfw': string
+    '@_xmlns:dc': string
+    '@_xmlns:atom': string
+    '@_version': string
+    '@_xmlns:sy': string
+    '@_xmlns:slash': string
+    channel: {
+      title: string
+      link: string
+      description: string
+      lastBuildDate: string
+      generator: string
+      item: {
+        title: string | null
+        link: string
+        comments: string
+        pubDate: string
+        category: string
+        guid: string
+        description: string
+        'content:encoded': string | null
+        'wfw:commentRss': string
+        'slash:comments': string
+      }[]
+    }
+  }
+}
+
 async function translate(
   message: string,
   before: string,
@@ -12,7 +43,11 @@ async function translate(
   if (!GASUrl) {
     throw new Error('GAS_URL is not set')
   }
-  const response = await axios.post(
+  const response = await axios.post<{
+    response: {
+      result: string
+    }
+  }>(
     GASUrl,
     {
       before,
@@ -36,23 +71,28 @@ async function translate(
 
 async function main() {
   const githubChangeLogURL =
-    process.env.GITHUB_CHANGELOG_URL || 'https://github.blog/changelog/feed/'
-  const beforeLanguage = process.env.BEFORE_LANGUAGE || 'en'
-  const afterLanguage = process.env.AFTER_LANGUAGE || 'ja'
+    process.env.GITHUB_CHANGELOG_URL ?? 'https://github.blog/changelog/feed/'
+  const beforeLanguage = process.env.BEFORE_LANGUAGE ?? 'en'
+  const afterLanguage = process.env.AFTER_LANGUAGE ?? 'ja'
   const outputChangeLogPath =
-    process.env.OUTPUT_CHANGELOG_PATH || 'output/changelog.xml'
+    process.env.OUTPUT_CHANGELOG_PATH ?? 'output/changelog.xml'
 
   const parser = new XMLParser({
     ignoreAttributes: false,
   })
   const response = await axios.get(githubChangeLogURL)
-  const oldFeed = parser.parse(response.data)
+  const oldFeed: Feed = parser.parse(response.data)
   for (const item of oldFeed.rss.channel.item) {
     const title = item.title
+    const content = item['content:encoded']
+    if (!title || !content) {
+      continue
+    }
 
     item.title = await translate(title, beforeLanguage, afterLanguage)
+
     item['content:encoded'] = await translate(
-      item['content:encoded'],
+      content,
       beforeLanguage,
       afterLanguage
     )
@@ -60,7 +100,9 @@ async function main() {
   const builder = new XMLBuilder({
     ignoreAttributes: false,
   })
-  const feed = builder.build(oldFeed)
+  const feed: {
+    toString: () => string
+  } = builder.build(oldFeed)
 
   const outputDirectory = path.dirname(outputChangeLogPath)
   if (!fs.existsSync(outputDirectory)) {
